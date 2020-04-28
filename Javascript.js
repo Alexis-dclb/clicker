575e4b8aa0fabeb07a779de6e6bd6a6fc9f93261
Alexis Ducoulombier <alexisducoulombier@hotmail.fr>
2020-04-28 13:52:23 +0200

    multiplicateur

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,8 @@
-var vinyl=document.getElementById("vinyl")
- 
+var vinyl=document.getElementById("vinyl");
+var ATotalCookie=document.getElementById("count");
+var AMultiplicateur=document.getElementById('compteur_multiplicateur');
+
+
 var angle=0;
 setInterval(function(){
     vinyl.style.transform="rotateZ("+ angle++ +"deg)";
@@ -7,8 +10,41 @@ setInterval(function(){
 
 
 var compteur = 0;
-function modifier(increment) {
-compteur+=increment;
-document.getElementById('count').value=compteur;
+function augmenterNombreCompteur() {
+compteur+=1*AMultiplicateur.innerHTML;
+ATotalCookie.textContent=compteur;
+console.log(compteur)
+
 }
+
+var multiplicateur = 1;
+var prixPardefaut= 50;
+var prix = 0;
+function augmenterMultiplicateur(increment){
+
+/*******cout de l'augmentation******/
+if(multiplicateur == 1){
+    prix = prixPardefaut;
+}
+else if(multiplicateur>1){
+    prix =  2*multiplicateur*prix;
+}
+
+if(compteur>=prix){
+    
+/******Incrementation a chaque click******/
+multiplicateur+=increment;
+AMultiplicateur.textContent=multiplicateur;
+
+compteur = ATotalCookie.innerHTML = ATotalCookie.innerHTML - prix;
+
+}
+else{
+    alert("trop pauvre le prix est de "+prix)
+}
+
+
+}
+
+
 
